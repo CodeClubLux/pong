6f0161706f1414f994784d04a25d18212bc80cda
Your Name <you@example.com>
2015-02-19 19:45:28 +0100

    - finished, needs font implementation, some bugs with racket

--- spritefile.py
+++ spritefile.py
@@ -1,19 +1,21 @@
+#!#/usr/bin/env python
 import pygame
 from pygame import *
 from random import randint
 class ball(pygame.sprite.Sprite):
 	def __init__(self, w, h):
 		super().__init__()
-		self.width=60
-		self.height=60
+		self.width=21
+		self.height=21
 		self.x=randint(0, w-self.width)
 		self.y=randint(0, h-self.height)
-		self.vx=3
-		self.vy=3
-		self.x_max = w
-		self.y_max = h
+		self.vx=1
+		self.vy=1
+		self.x_max = w-self.width
+		self.y_max = h-self.height
 		color = white
 		self.image = pygame.Surface([self.width, self.height])
+		#self.image = pygame.draw.circle(Surface, (255, 255, 255), (400, 300), 15, 0)
 		self.image.fill(color)
 		self.rect = self.image.get_rect()
 		self.move_right = True
@@ -35,19 +37,26 @@ class ball(pygame.sprite.Sprite):
 		#print (self.x,self.y,self.vx,self.vy) 
 		self.rect.x = self.x
 		self.rect.y = self.y
+		self.vx *=0.9999
+		self.vy *=0.9999
 
-	def bounce(self):
-		self.vx = - self.vx
-		self.vy = - self.vy
-
+	def bounce(self, rvx, rvy):
+		self.vx = - self.vx - rvx/100
+		self.vy = self.vy - rvy/100 
+		if abs(self.vx) > 4 :
+			self.vx = 4
+		if abs(self.vy) > 4	:
+			self.vy = 4
+		print('%.2f\t%.2f'%(self.vx, self.vy))
+		
 class racket(pygame.sprite.Sprite):
 	def __init__(self, x, y, color):
 		super().__init__()
 		pygame.mouse.set_visible(0)
 		self.x=x
 		self.y=y
-		self.width=60
-		self.height=120
+		self.width=20
+		self.height=220
 		self.image = pygame.Surface([self.width, self.height])
 		self.image.fill(color)
 		self.rect = self.image.get_rect()
@@ -57,8 +66,10 @@ class racket(pygame.sprite.Sprite):
 		
 	def update(self):
 		self.mouse_xy = pygame.mouse.get_pos() 
-		self.x = self.mouse_xy[0]
-		self.y = self.mouse_xy[1]
+		self.vx = -self.x + self.mouse_xy[0]
+		self.vy = -self.y + self.mouse_xy[1]
+		self.x += self.vx/100
+		self.y += self.vy/100
 		#if self.x>=self.x_max:
 		#	self.vx=-self.vx
 		#if self.y>=self.y_max: 
@@ -70,14 +81,14 @@ class racket(pygame.sprite.Sprite):
 		#print (self.x,self.y,self.vx,self.vy)
 		self.rect.x = self.x
 		self.rect.y = self.y
+		#if self.vx != 0 or self.vy != 0:
+		#print(self.vx, self.vy)
 		
-
 class target(pygame.sprite.Sprite):
 	def __init__(self, x, y, color=(255,0,0)):
 		super().__init__()
-		
-		self.x=x
-		self.y=y
+		#self.x=x
+		#self.y=y
 		self.width=30
 		self.height=30
 		self.x=randint(0, x-self.width)
@@ -87,9 +98,22 @@ class target(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.x = self.x
 		self.rect.y = self.y
-		print(self.x)
-		print(self.y)
-	#def create(self, x, y, color= (255, 0, 0)
+		self.hit = False
+		#print(self.x)
+		#print(self.y)
+		
+	def update(self):
+		if self.hit is True:
+			self.x = randint(0, 800-self.width)
+			self.y = randint(0, 600-self.height)
+			self.image = pygame.Surface([self.width, self.height])
+			self.image.fill((randint(0, 255),randint(0, 255),randint(0, 255)))
+			self.rect = self.image.get_rect()
+			self.rect.x = self.x
+			self.rect.y = self.y
+			self.hit = False
+		#if all_sprites.remove(target) = True:
+		#def create(self, x, y, color= (255, 0, 0)
 
 if __name__=="__main__":
 	import pygame
@@ -102,25 +126,44 @@ if __name__=="__main__":
 	ball = ball(800,600)
 	racket = racket(400,300,(50,150,255))
 	target = target(400,300)
+	bounced = 0
+	score = 0
+	time = 0
 	all_sprites = pygame.sprite.Group()
 	all_sprites.add(ball)
 	all_sprites.add(racket)
 	all_sprites.add(target)
 	background = pygame.Surface(window.get_size())
-	
+	window.fill(black)
 	while True:
+		time += 1
 		for i in pygame.event.get():
 			if i.type == QUIT:
 				exit()
-		window.fill(black)
+		
 		all_sprites.update()
+		#if bounced > 0:
+		#	bounced -= 1
+		if pygame.sprite.collide_rect(ball, racket): 
+		#pygame.sprite.spritecollide(ball, all_sprites, False) and (bounced <= 0):
+			#spritecollide(sprite, group, dokill, collided = None)
+			ball.bounce(racket.vx, racket.vy)
+			#bounced = 5
+			#print('%.2f\t%.2f\t%d\t%d' % (ball.x, ball.y, racket.x, racket.y))
+		if pygame.sprite.collide_rect(ball, target):  
+		#pygame.sprite.spritecollide(target, all_sprites, False):
+		    #all_sprites.remove(target)
+		    target.hit = True
+		    #print("I have been hit!")
+		    score += 1
+		    print(score)
 
-		if pygame.sprite.collide_rect(ball, racket):
-		    ball.bounce()
-		if pygame.sprite.collide_rect(ball, target):
-		    all_sprites.remove(target)
-		    print("I have been hit!")
+			
+		    
 
-		all_sprites.draw(window)
-		clock.tick(30)
+		if time == 10:
+			window.fill(black)
+			all_sprites.draw(window)
+			time = 0
+		clock.tick(300)
 		pygame.display.flip()
